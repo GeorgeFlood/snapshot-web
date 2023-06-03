3fec04274f32307e95d73c9d08bb0bfb761e3d2d
GeorgeFlood <mrgeorgeflood@gmail.com>
2023-06-03 19:37:52 +0100

    features page

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "../shared/Navbar";
-
+import Header from "../features/Header";
+import Footer from "../shared/Footer";
 const Features = () => {
   return (
     <div>
       <Navbar />
+      <Header />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/features/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import arrow from "../../assets/shared/desktop/arrow.svg";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header--text">
+        <h4>LAST MONTH'S FEATURED STORY</h4>
+        <h1>HAZY FULL MOON OF APPALACHIA</h1>
+        <p className="header--text__credit">March 2nd 2020 by John Appleseed</p>
+        <p className="header--text__para">
+          The dissected plateau area, while not actually made up of geological
+          mountains, is popularly called "mountains," especially in eastern
+          Kentucky and West Virginia, and while the ridges are not high, the
+          terrain is extremely rugged.
+        </p>
+        <div className="header--story">
+          <p>READ STORY</p>
+          <img src={arrow} alt="arrow" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
