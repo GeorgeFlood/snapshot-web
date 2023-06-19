d54a8c829b3e812ffe684617de380fe84ca6b2dd
GeorgeFlood <mrgeorgeflood@gmail.com>
2023-06-19 19:54:28 +0100

    trying to fix the home page invite sections

--- src/components/stories/Header.jsx
+++ src/components/stories/Header.jsx
@@ -1,85 +1,65 @@
-export const Header = () => {
-  return (
-    <div className="header">
-      <div className="header__container">
-        <div className="header__info">
-          <h1 className="header__info--title">
-            CREATE AND SHARE YOUR PHOTO STORIES
-            <span className="design-line"></span>
-          </h1>
-          <p className="header__info--para">
-            Photosnap is a platform for photographers and visual storytellers.
-            We make it easy to share photos, tell stories and connect with
-            others.
-          </p>
-          <div className="header__info--invite">
-            <p>GET AN INVITE</p>
-            <svg
-              className="header__info--arrow"
-              xmlns="http://www.w3.org/2000/svg"
-              width="43"
-              height="14"
-            >
-              <g fill="none" fill-rule="evenodd" stroke="#000">
-                <path d="M0 7h41.864M35.428 1l6 6-6 6" />
-              </g>
-            </svg>
-          </div>
-        </div>
-        <div className="header__img"></div>
-      </div>
+// Define smaller components to be reused
+const HeaderInfo = ({ title, para, inviteColor, arrowColor }) => (
+  <div className="header__info">
+    <h1 className="header__info--title">{title}</h1>
+    <p className="header__info--para">{para}</p>
+    <InviteButton inviteColor={inviteColor} arrowColor={arrowColor} />
+  </div>
+);
 
-      <div className="header__container second-container">
-        <div className="header__img"></div>
-        <div className="header__info">
-          <h1 className="header__info--title">BEAUTIFUL STORIES EVERY TIME</h1>
-          <p className="header__info--para">
-            We provide design templates to ensure your stories look terrific.
-            Easily add photos, text, embed maps and media from other networks.
-            Then share your story with everyone.
-          </p>
-          <div className="header__info--invite">
-            <p>GET AN INVITE</p>
-            <svg
-              className="header__info--arrow"
-              xmlns="http://www.w3.org/2000/svg"
-              width="43"
-              height="14"
-            >
-              <g fill="none" fill-rule="evenodd" stroke="#000">
-                <path d="M0 7h41.864M35.428 1l6 6-6 6" />
-              </g>
-            </svg>
-          </div>
-        </div>
-      </div>
+const InviteButton = ({ inviteColor, arrowColor }) => (
+  <div className={`header__info--invite ${inviteColor}`}>
+    <p>GET AN INVITE</p>
+    <Arrow arrowColor={arrowColor} />
+  </div>
+);
 
-      <div className="header__container third-container">
-        <div className="header__info">
-          <h1 className="header__info--title">DESIGNED FOR EVERYONE</h1>
-          <p className="header__info--para">
-            Photosnap is a platform for photographers and visual storytellers.
-            We make it easy to share photos, tell stories and connect with
-            others.
-          </p>
-          <div className="header__info--invite">
-            <p>GET AN INVITE</p>
-            <svg
-              className="header__info--arrow"
-              xmlns="http://www.w3.org/2000/svg"
-              width="43"
-              height="14"
-            >
-              <g fill="none" fill-rule="evenodd" stroke="#000">
-                <path d="M0 7h41.864M35.428 1l6 6-6 6" />
-              </g>
-            </svg>
-          </div>
-        </div>
-        <div className="header__img"></div>
-      </div>
+const Arrow = ({ arrowColor }) => (
+  <svg
+    className={`header__info--arrow ${arrowColor}`}
+    xmlns="http://www.w3.org/2000/svg"
+    width="43"
+    height="14"
+  >
+    <g fill="none" fill-rule="evenodd" stroke="#000">
+      <path d="M0 7h41.864M35.428 1l6 6-6 6" />
+    </g>
+  </svg>
+);
+
+// Simplified main component
+export const Header = () => (
+  <div className="header">
+    <div className="header__container">
+      <HeaderInfo
+        title="CREATE AND SHARE YOUR PHOTO STORIES"
+        para="Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others."
+        inviteColor="white"
+        arrowColor="white"
+      />
+      <div className="header__img"></div>
     </div>
-  );
-};
+
+    <div className="header__container second-container">
+      <div className="header__img"></div>
+      <HeaderInfo
+        title="BEAUTIFUL STORIES EVERY TIME"
+        para="We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone."
+        inviteColor="black"
+        arrowColor="black"
+      />
+    </div>
+
+    <div className="header__container third-container">
+      <HeaderInfo
+        title="DESIGNED FOR EVERYONE"
+        para="Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others."
+        inviteColor="black"
+        arrowColor="black"
+      />
+      <div className="header__img"></div>
+    </div>
+  </div>
+);
 
 export default Header;
